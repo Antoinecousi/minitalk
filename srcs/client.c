f0c78d29574378b32b4b26e6bfcdcfb4b05f6d04
Antoine COUSINIE <acousini@e1r12p5.42.fr>
2021-12-08 15:39:47 +0100

    PingPong added

--- srcs/client.c
+++ srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: acousini <acousini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 19:13:49 by acousini          #+#    #+#             */
-/*   Updated: 2021/12/03 17:54:15 by acousini         ###   ########.fr       */
+/*   Updated: 2021/12/08 15:37:34 by acousini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,34 @@ void	write_input(char *str, int pid)
 			else
 				kill(pid, SIGUSR1);
 			j++;
-			usleep(150);
+			usleep(150);//is this needed?
+			pause();
 		}
 		i++;
 	}
 }
 
+void	handler(int signal)
+{
+	if (signal == SIGUSR2)
+		exit(1);
+}
+
 int	main(int argc, char **argv)
 {
-	char	*str;
+	char				*str;
+	struct sigaction	sig;
 
+	sig.sa_flags = SA_RESTART;
+	sig.sa_handler = handler;
 	if (argc != 3)
 	{
 		ft_putstr_fd("Usage is : \t./client \"Server PID\" \"String\"", 1);
 		return (0);
 	}
 	str = argv[2];
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
 	write_input(str, ft_atoi(argv[1]));
 	return (0);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: acousini <acousini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 17:12:39 by acousini          #+#    #+#             */
-/*   Updated: 2021/12/06 17:18:47 by acousini         ###   ########.fr       */
+/*   Updated: 2021/12/08 15:38:34 by acousini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 static t_strct	g_input;
 
-void	handle_sigusr1(int d)
+void	handle_sigusr1(int byte_received, int pid)
 {
-	(void)d;
+	(void)byte_received;
 	g_input.size++;
 	if (g_input.size >= 7)
 	{
@@ -24,11 +24,13 @@ void	handle_sigusr1(int d)
 		g_input.size = 0;
 		g_input.c = 0;
 	}
+	usleep(500);
+	kill(pid, SIGUSR1);
 }
 
-void	handle_sigusr2(int d)
+void	handle_sigusr2(int byte_received, int pid)
 {
-	(void)d;
+	(void)byte_received;
 	g_input.c += 1 << g_input.size;
 	g_input.size++;
 	if (g_input.size >= 7)
@@ -37,21 +39,36 @@ void	handle_sigusr2(int d)
 		g_input.size = 0;
 		g_input.c = 0;
 	}
+	usleep(500);
+	kill(pid, SIGUSR1);
+}
+
+static void	handler(int signal, siginfo_t *info, void *context)
+{
+	(void)context;
+	if (signal == SIGUSR1)
+		handle_sigusr1(0, info->si_pid);
+	else if (signal == SIGUSR2)
+		handle_sigusr2(1, info->si_pid);
 }
 
 int	main(int argc, char **argv)
 {
+	struct sigaction	sig;
+
+	(void)argv;
 	if (argc != 1)
 	{
 		ft_putstr_fd("For correct behaviour, don't use any arguments\n", 1);
 		return (0);
 	}
-	(void)argv;
+	sig.sa_flags = SA_SIGINFO | SA_RESTART;
+	sig.sa_sigaction = handler;
 	write(1, "pid = ", 6);
 	ft_putnbr_fd(getpid(), 1);
 	write(1, "\n", 1);
-	signal(SIGUSR1, handle_sigusr1);
-	signal(SIGUSR2, handle_sigusr2);
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
 	while (1)
 		pause();
 	return (0);
